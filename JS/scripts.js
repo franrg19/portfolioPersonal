b6509093ab7fd01d2ee9ef969dca8b869da8be99
franrg19 <franrg19@outlook.com>
2025-07-30 20:17:01 +0200

    Añadido pop up cookies

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,3 +4,21 @@ const toggle = document.getElementById("menu-toggle");
   toggle.addEventListener("click", () => {
     menu.classList.toggle("show");
   });
+
+
+  /* cookies*/
+
+  document.addEventListener('DOMContentLoaded', () => {
+  const popup = document.getElementById('cookie-popup');
+  const acceptBtn = document.getElementById('accept-cookies');
+
+  // Comprobar si ya aceptó
+  if (!localStorage.getItem('cookiesAccepted')) {
+    popup.style.display = 'flex';
+  }
+
+  acceptBtn.addEventListener('click', () => {
+    localStorage.setItem('cookiesAccepted', 'true');
+    popup.style.display = 'none';
+  });
+});
